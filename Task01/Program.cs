a5bb84bcb96a5d2ae86cb8c378d6ed934c146409
artem.chernomazov <pechenkazz@mail.ru>
2022-09-01 01:00:16 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число А в натуральную степень В*/
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int c=1;
+for(int i = 1; i<=b; i++)
+{
+    c=c*a;
+}
+Console.WriteLine(c);
